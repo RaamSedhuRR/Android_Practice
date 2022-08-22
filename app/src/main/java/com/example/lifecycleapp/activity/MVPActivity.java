aab4a406a8f0049837e792dc228ade770ab23244
RRRaamSedhu <rrraamcena2000@gmail.com>
2022-08-22 19:11:47 +0530

    Backup

---
+++ app/src/main/java/com/example/lifecycleapp/activity/MVPActivity.java
@@ -0,0 +1,14 @@
+package com.example.lifecycleapp.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MVPActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mvpactivity);
+    }
+}
---
+++ app/src/main/java/com/example/lifecycleapp/mvpArchitecture/LoginPresenter.java
@@ -0,0 +1,2 @@
+package com.example.lifecycleapp.mvpArchitecture;public class LoginPresenter {
+}
